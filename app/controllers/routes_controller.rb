763c4127abb3b7cb7bf300d019a1c1b7e994cf2a
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-09-25 14:06:52 +0500

    Added actions new, create

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -2,4 +2,23 @@ class RoutesController < ApplicationController
   def index
     @routes = Route.all
   end
+
+  def new
+    @route = Route.new
+  end
+
+  def create
+    @route = Route.new(route_params)
+    if @route.save
+      redirect_to @route, notice: 'Route was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def route_params
+    params.require(:route).permit(:name)
+  end
 end
